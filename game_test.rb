59ff2713beb039449a10eb87f048f5d4311551d9
Jack Anderson <jja54@pitt.edu>
2019-02-12 21:59:51 -0500

    test case additions

--- game_test.rb
+++ game_test.rb
@@ -9,6 +9,11 @@ require_relative 'town'
 
 # This is a MiniTest test file. It will test the Game class in ruby_rush.rb
 class GameTest < MiniTest::Test
+  # UNIT TEST FOR METHOD populate_world
+  # populate_world should return the same
+  # start town with the same start data every time.
+  # As such, it does not lend itself to equivalence
+  # class partioning
   def test_populate_world
     game = Game.new
     start_town = game.populate_world
@@ -19,33 +24,66 @@ class GameTest < MiniTest::Test
     assert_equal 1, start_town.fake_rubies
     assert_equal 2, start_town.adjacent_towns.count
   end
+  # ------------------------------------------
 
-  def test_prospect
-    game = Game.new
-    rubies_count = 2
-    town = Town.new('Dynamic Palisades', rubies_count, rubies_count)
-    prng = Random.new(seed * prospect_num)
-    rubies = game.prospect(town, prng)
+  # UNIT TESTS FOR METHOD prospect(town, prng)
+  # Equivalence classes:
+  # x= -INFINITY..-1 -> returns -x
+  # x= 0..INFINITY -> returns x
+  # x= (not a number) -> returns nil
 
-    assert_not_nil rubies
-    assert_equal rubies.size, 2
-    assert_block do
-      rubies.any? { |num| num > -1 && num <= rubies_count }
-    end
-  end
-  
-  def test_move_town
+  # The rubies array returned should not be nil
+  def test_prspct_rubies_not_nil
     game = Game.new
-    rubies_count = 2
     town = game.populate_world
-    prng = Random.new(seed * prospect_num)
-    next_town = game.move_town(town, prng)
-
-    assert_block do
-      town.adjacent_towns.any? { |town| town == next_town }
-    end
-    assert next_town.rubies >= 0
-    assert next_town.fake_rubies >= 0
-    assert next_town.adjacent_towns.count > 0
+    prng = Random.new
+    rubies = game.prospect(town, prng)
+	refute_nil rubies
   end
+
+  # The rubies[0] variable returned should be >= 0 and
+  # not exceed the amount of rubies specified for the town
+  def test_prspct_rubies_count
+    game = Game.new
+    town = game.populate_world
+    prng = Random.new
+    rubies = game.prospect(town, prng)
+	assert(rubies[0] > -1 && rubies[0] <= town.rubies) 
+  end
+
+  # The rubies[1] variable returned should be >= 0 and
+  # not exceed the amount of fake rubies specified for the town
+  def test_prspct_fake_rubies_count
+    game = Game.new
+    town = game.populate_world
+    prng = Random.new
+    rubies = game.prospect(town, prng)
+	assert(rubies[1] > -1 && rubies[1] <= town.fake_rubies) 
+  end
+  # ------------------------------------------
+
+  # UNIT TESTS FOR METHOD move_town(town, prng)
+  # Equivalence classes:
+  # town= (valid town) -> returns (any in town.adjacent_towns)
+
+  # The next town chosen should be one of the towns
+  # in the current town's adjacent_towns list
+  def test_move_is_valid_town
+    game = Game.new
+    town = game.populate_world
+    prng = Random.new
+    next_town = game.move_town(town, prng)
+    assert(town.adjacent_towns.any? { |adj_town| adj_town == next_town })
+  end
+  # The next town chosen should allow for travel back
+  # to the town from which it came (undirected graph)
+  def test_move_back_possible
+    game = Game.new
+    town = game.populate_world
+    prng = Random.new
+    next_town = game.move_town(town, prng)
+    assert(next_town.adjacent_towns.any? { |adj_town| adj_town == town })
+  end
+  # ------------------------------------------
+
 end
